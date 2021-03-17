9373a242b118cb62e6b5249b373767949b8b2ad3
venkadeshbabu06 <80838306+venkadeshbabu06@users.noreply.github.com>
2021-03-17 20:47:59 +0530

    Add files via upload

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // x=1;
+            // sonar qube should find the above line as code smell.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
